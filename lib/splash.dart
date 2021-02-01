1d584c9df4264bde7b41ab7e1cd65bde7dab77ad
akillika <akillika49@gmail.com>
2021-02-01 12:39:35 +0530

    Splash screen and home page started

---
+++ lib/splash.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class MyApp1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Solution Challenge',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Solution Challenge'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  String _versionName = 'V1.0';
+  final splashDelay = 3;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _loadWidget();
+  }
+
+  _loadWidget() async {
+    var _duration = Duration(seconds: splashDelay);
+    return Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => MyApp()));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink,
+
+      body: Center(child: CircularProgressIndicator(backgroundColor: Colors.white,))
+    );
+  }
+}
+
+String greetings() {
+  var now;
+  now = DateTime.now().hour;
+  if (now < 12) {
+    return 'Good morning, ';
+  } else if (now < 17) {
+    return 'Good afternoon,';
+  } else {
+    return 'Good evening,';
+  }
+}
