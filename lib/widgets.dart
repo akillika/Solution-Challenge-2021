cebcdb694fa786710a72198968c3f3d85fa3ef1f
akillika <akillika49@gmail.com>
2021-03-02 20:37:52 +0530

    App icon and drawer changes

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -9,143 +9,143 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 30,
-          ),
-          DrawerHeader(
-            child: Container(
-                height: 142,
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 30,
+            ),
+            DrawerHeader(
+              child: Container(
+                  height: 142,
+                  width: MediaQuery.of(context).size.width,
+                  child: Image.asset(
+                    "assets/drawer.png",
+                    fit: BoxFit.scaleDown,
+                  )),
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => MyHomePage()));
+              },
+              child: Text(
+                'Dashboard',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => Category(
+                          type: 'Donate',
+                        )));
+              },
+              child: Text(
+                'Donate',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => Category(
+                          type: 'Request Donation',
+                        )));
+              },
+              child: Text(
+                'Request Donation',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            // SizedBox(
+            //   height: 45,
+            // ),
+            // Text(
+            //   'About us',
+            //   style: TextStyle(
+            //     fontFamily: 'Avenir',
+            //     fontSize: 24,
+            //     fontWeight: FontWeight.w700,
+            //   ),
+            //   textAlign: TextAlign.center,
+            // ),
+            SizedBox(
+              height: 45,
+            ),
+            GestureDetector(
+              onTap: () {
+                signOutGoogle().then((result) {
+                  Navigator.pushAndRemoveUntil<dynamic>(
+                    context,
+                    MaterialPageRoute<dynamic>(
+                      builder: (BuildContext context) => LoginPage(),
+                    ),
+                    (route) => false,
+                  );
+                });
+              },
+              child: Text(
+                'Logout',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 45,
+            ),
+
+            Expanded(
+                child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 65,
                 width: MediaQuery.of(context).size.width,
-                child: Image.asset(
-                  "assets/dsclogo.png",
-                )),
-            decoration: BoxDecoration(
-              color: Colors.white,
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext context) => MyHomePage()));
-            },
-            child: Text(
-              'Dashboard',
-              style: TextStyle(
-                fontFamily: 'Avenir',
-                fontSize: 24,
-                fontWeight: FontWeight.w700,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          SizedBox(
-            height: 45,
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => Category(
-                        type: 'Donate',
-                      )));
-            },
-            child: Text(
-              'Donate',
-              style: TextStyle(
-                fontFamily: 'Avenir',
-                fontSize: 24,
-                fontWeight: FontWeight.w700,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          SizedBox(
-            height: 45,
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => Category(
-                        type: 'Request Donation',
-                      )));
-            },
-            child: Text(
-              'Request Donation',
-              style: TextStyle(
-                fontFamily: 'Avenir',
-                fontSize: 24,
-                fontWeight: FontWeight.w700,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          SizedBox(
-            height: 45,
-          ),
-          Text(
-            'About us',
-            style: TextStyle(
-              fontFamily: 'Avenir',
-              fontSize: 24,
-              fontWeight: FontWeight.w700,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(
-            height: 45,
-          ),
-          GestureDetector(
-            onTap: () {
-              signOutGoogle().then((result) {
-                Navigator.pushAndRemoveUntil<dynamic>(
-                  context,
-                  MaterialPageRoute<dynamic>(
-                    builder: (BuildContext context) => LoginPage(),
+                color: Colors.pink,
+                child: Center(
+                  child: Text(
+                    'Developed by Akil & Madhavan',
+                    style: TextStyle(color: Colors.white),
                   ),
-                  (route) => false,
-                );
-              });
-            },
-            child: Text(
-              'Logout',
-              style: TextStyle(
-                fontFamily: 'Avenir',
-                fontSize: 24,
-                fontWeight: FontWeight.w700,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          SizedBox(
-            height: 45,
-          ),
-          Expanded(
-              child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: 65,
-              width: MediaQuery.of(context).size.width,
-              color: Colors.pink,
-              child: Center(
-                child: Text(
-                  'v1.0.1',
-                  style: TextStyle(
-                    fontFamily: 'Avenir',
-                    fontSize: 20,
-                    color: const Color(0xffffffff),
-                  ),
-                  textAlign: TextAlign.center,
                 ),
               ),
-            ),
-          ))
-        ],
+            ))
+          ],
+        ),
       ),
     );
   }
